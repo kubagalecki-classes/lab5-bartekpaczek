797d5cbb306d2c70327737824b9dcde63794b73f
Bartek <bartek.paczek@gmail.com>
2020-12-08 21:54:34 +0100

    zad11

--- lab5.cpp
+++ lab5.cpp
@@ -1,31 +1,39 @@
 #include <iostream>
 #include<vector>
+#include<list>
 #include<string>
 #include"Human.hpp"
 
 using namespace std;
 
 int main()
-{
-    string name;
-    vector<Human> grupa;
-    while(1){
-        cout<<"Podaj imie:";
-        cin>>name;
-        
-        if(name.compare("pop")!=0){
-            Human osoba{name, 20, true, true};
-            grupa.emplace_back(move(osoba));
-            cout<<"dodano osobe do grupy.\n";
-            
-        } else if(name.compare("pop")==0 && grupa.size()!=0) {
-            grupa.pop_back();
-            cout<<"Usunieto osobe z grupy.\n";
-        };
-        
-        for(int i=0;i<grupa.size();i++){
-            cout<<grupa[i].getName()<<"\n";
-        };
+{   //Zad6
+    //string name;
+    //vector<Human> grupa;
+    //while(1){
+    //    cout<<"Podaj imie:";
+    //    cin>>name;
+    //    
+    //    if(name.compare("pop")!=0){
+    //        Human osoba{name, 20, true, true};
+    //        grupa.emplace_back(move(osoba));
+    //        cout<<"dodano osobe do grupy.\n";
+    //        
+    //    } else if(name.compare("pop")==0 && grupa.size()!=0) {
+    //        grupa.pop_back();
+    //        cout<<"Usunieto osobe z grupy.\n";
+    //    };
+    //    
+    //    for(int i=0;i<grupa.size();i++){
+    //        cout<<grupa[i].getName()<<"\n";
+    //    };
+//
+    //}
+    list<int> dupa;
 
-    }
+    dupa.push_back(1);
+    dupa.push_back(2);
+    cout<<&dupa.front()<<"\n";
+    cout<<&dupa.back();
+
 }
