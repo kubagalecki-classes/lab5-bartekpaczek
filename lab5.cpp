56a64929dc7491528ebc59daac5f6108d857d256
Bartek <bartek.paczek@gmail.com>
2020-12-08 21:40:10 +0100

    zad6

--- lab5.cpp
+++ lab5.cpp
@@ -1,14 +1,31 @@
 #include <iostream>
 #include<vector>
+#include<string>
+#include"Human.hpp"
+
 using namespace std;
 
 int main()
 {
-    vector<int> a;
-    cout<<"Size: "<<a.size()<<"\nCapacity: "<<a.capacity();
-    for(int i=0;i<100;i++){
-        a.emplace_back(i);
-        cout<<"\n\nSize: "<<a.size()<<"\nCapacity: "<<a.capacity();
-        cout<<"\nAddress: "<<&a[0];
+    string name;
+    vector<Human> grupa;
+    while(1){
+        cout<<"Podaj imie:";
+        cin>>name;
+        
+        if(name.compare("pop")!=0){
+            Human osoba{name, 20, true, true};
+            grupa.emplace_back(move(osoba));
+            cout<<"dodano osobe do grupy.\n";
+            
+        } else if(name.compare("pop")==0 && grupa.size()!=0) {
+            grupa.pop_back();
+            cout<<"Usunieto osobe z grupy.\n";
+        };
+        
+        for(int i=0;i<grupa.size();i++){
+            cout<<grupa[i].getName()<<"\n";
+        };
+
     }
 }
