8233c3317816365e4a04b050820f38691cb20c5c
Bartek <bartek.paczek@gmail.com>
2020-12-08 22:30:14 +0100

    zal 1

--- lab5.cpp
+++ lab5.cpp
@@ -8,32 +8,25 @@ using namespace std;
 
 int main()
 {   //Zad6
-    //string name;
-    //vector<Human> grupa;
-    //while(1){
-    //    cout<<"Podaj imie:";
-    //    cin>>name;
-    //    
-    //    if(name.compare("pop")!=0){
-    //        Human osoba{name, 20, true, true};
-    //        grupa.emplace_back(move(osoba));
-    //        cout<<"dodano osobe do grupy.\n";
-    //        
-    //    } else if(name.compare("pop")==0 && grupa.size()!=0) {
-    //        grupa.pop_back();
-    //        cout<<"Usunieto osobe z grupy.\n";
-    //    };
-    //    
-    //    for(int i=0;i<grupa.size();i++){
-    //        cout<<grupa[i].getName()<<"\n";
-    //    };
-//
-    //}
-    list<int> dupa;
+    string name;
+    vector<Human> grupa;
+    while(1){
+        cout<<"Podaj imie:";
+        cin>>name;
+        
+        if(name.compare("pop")!=0){
+            Human osoba{name, 20, true, true};
+            grupa.emplace_back(move(osoba));
+            cout<<"dodano osobe do grupy.\n";
+            
+        } else if(name.compare("pop")==0 && grupa.size()!=0) {
+            grupa.pop_back();
+            cout<<"Usunieto osobe z grupy.\n";
+        };
+        vector<Human>::reverse_iterator it = grupa.rbegin();
+        for(it;it<grupa.rend();it++){
+            cout<<it->getName()<<"\n";
+        };
 
-    dupa.push_back(1);
-    dupa.push_back(2);
-    cout<<&dupa.front()<<"\n";
-    cout<<&dupa.back();
-
+    }  
 }
--- zal/foo.hpp
+++ zal/foo.hpp
@@ -6,7 +6,18 @@
 #include <vector>
 
 std::vector< char > foo(std::list< Human >& people)
-{
-    // Twoja implementacja tutaj
-    return {};
+{   
+    std::vector< char > lubi;
+    std::list<Human>::reverse_iterator it;
+    for(it;it<people.rbegin();it++){
+        *it.birthday();
+        if(*it.isMonster()){
+            lubi.push_back('y');
+        }else
+        {
+           lubi.push_back('n');
+        }
+        
+    };
+
 }
