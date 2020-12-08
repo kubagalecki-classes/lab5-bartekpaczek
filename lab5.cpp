d255e20e25bbf633ae6ebeb5bd6cb865efe15881
Bartek <bartek.paczek@gmail.com>
2020-12-08 20:59:59 +0100

    zad2

--- lab5.cpp
+++ lab5.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include<vector>
+using namespace std;
 
 int main()
 {
-    puts("Hello, World!");
+    vector<int> a;
+    cout<<"Size: "<<a.size()<<"\nCapacity: "<<a.capacity();
+    for(int i=0;i<100;i++){
+        a.emplace_back(i);
+        cout<<"\n\nSize: "<<a.size()<<"\nCapacity: "<<a.capacity();
+        cout<<"\nAddress: "<<&a[0];
+    }
 }
